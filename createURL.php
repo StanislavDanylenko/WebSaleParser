b1710471434ab53b9f0ca840af8accb8e906a50d
StanislavDanylenko <38065736+StanislavDanylenko@users.noreply.github.com>
2018-04-10 11:31:27 +0300

    Fixed bugs

--- createURL.php
+++ createURL.php
@@ -3,25 +3,25 @@ function createURL(){
     global $URL_for_parsing;
     $URL = 'https://www.olx.ua/nedvizhimost/';
     // тип строения
-    if (/*проверить поле на сайте*/){
+    if (true){
         $URL = $URL.'doma/';
     } else {
         $URL = $URL.'kvartiry-komnaty/';
     }
 
     // тип операции
-    if (/*проверить поле на сайте*/) {
+    if (true) {
         $URL = $URL.'prodazha-domov/';
-    } else if (/*проверить поле на сайте*/){
+    } else if (true){
         $URL = $URL.'arenda-domov/';
-    } else if (/*проверить поле на сайте*/){
+    } else if (true){
         $URL = $URL.'prodazha-kvartir-komnat/';
-    } else if (/*проверить поле на сайте*/){
+    } else if (true){
         $URL = $URL.'arenda-kvartir-komnat/';
     }
 
     //если еще заданы какие-то поля
-    if (){
+    if (true){
         $URL = $URL.'?';
     }
 
@@ -32,37 +32,37 @@ function createURL(){
     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
     // этаж для квартиры
-    if (/* проверить поле на сайте*/){
+    if (true){
         $URL = $URL.'search%5Bfilter_float_floor%3Afrom%5D='.''; // дописать конкатанацию значения начального этажа
-    } else if (/* проверить поле на сайте*/){
+    } else if (true){
         $URL = $URL.'search%5Bfilter_float_floor%3Ato%5D='.''; // дописать конкатанацию значения последнего этажа
     }
 
     // этажность
-    if (/* проверить поле на сайте*/){
+    if (true){
         $URL = $URL.'search%5Bfilter_float_total_floors%3Afrom%5D='.''; // дописать
-    } else if (/* проверить поле на сайте*/){
+    } else if (true){
         $URL = $URL.'search%5Bfilter_float_total_floors%3Ato%5D='.''; // дописать
     }
 
     // количество комнат в квартире
-    if (/* проверить поле на сайте*/){
+    if (true){
         $URL = $URL.'search%5Bfilter_float_number_of_rooms%3Afrom%5D='.''; // дописать
-    } else if (){
+    } else if (true){
         $URL = $URL.'search%5Bfilter_float_number_of_rooms%3Ato%5D='.''; // дописать
     }
 
     // общая площадь
-    if (/* проверить поле на сайте*/){
+    if (true){
         $URL = $URL.'search%5Bfilter_float_total_area%3Afrom%5D='.''; // дописать
-    } else if (){
+    } else if (true){
         $URL = $URL.'search%5Bfilter_float_total_area%3Ato%5D=5'.''; // дописать
     }
 
     // площадь участка (для ДОМА)
-    if (/* проверить поле на сайте*/){
+    if (true){
         $URL = $URL.'search%5Bfilter_float_land_area%3Afrom%5D='.''; // дописать
-    } else if (){
+    } else if (true){
         $URL = $URL.'search%5Bfilter_float_land_area%3Ato%5D='.''; // дописать
     }
 
